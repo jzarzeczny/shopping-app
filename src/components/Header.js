17a6f28aceb0541bf53e2e650b59fd33986ceb4c
jzarzeczny <jkzarzeczny@gmail.com>
2021-09-07 18:34:22 +0200

    mobile add

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <p>Logo</p>
       <Navbar />
     </header>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
   return (
-    <nav className="navigation">
-      <button className="hamburger">
+    <nav className={`nav ${open && "nav--open"}`}>
+      <button className="hamburger" onClick={toggle}>
         <div className="hamburger-line"></div>
       </button>
       <ul className="nav__list">
-        <li className="nav__element"></li>
-        <li className="nav__element"></li>
-        <li className="nav__element"></li>
-        <li className="nav__element"></li>
+        <li className="nav__element">Strona główna</li>
+        <li className="nav__element">Lista zakupów</li>
+        <li className="nav__element">Stwórz listę</li>
       </ul>
     </nav>
   );

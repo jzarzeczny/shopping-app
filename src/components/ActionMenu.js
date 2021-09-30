ad5aaa0bb1a05a10d71ea31c8b45a255c3478164
jzarzeczny <jkzarzeczny@gmail.com>
2021-09-30 11:10:01 +0200

    hisotry update

---
+++ src/components/ActionMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ActionMenu({ active }) {
+  return (
+    <div className="actionMenu">
+      <ul className={`action__list ${active ? "actionMenu--active" : null}`}>
+        <li className="action__element">Edytuj</li>
+        <li className="action__element">Ponów koszyk</li>
+        <li className="action__element">Dodaj kwotę</li>
+      </ul>
+    </div>
+  );
+}
--- src/components/HistoryRow.js
+++ src/components/HistoryRow.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import ActionMenu from "./ActionMenu";
 
 export default function HistoryRow({ data }) {
+  const [toggleActionMenu, setToggleActionMenu] = useState(false);
   function countListItems(list) {
     let finalNumber = 0;
 
@@ -13,13 +15,31 @@ export default function HistoryRow({ data }) {
   }
 
   return (
-    <tr className="table__row">
-      <td className="table__data">
-        {new Date(data.date).toLocaleDateString("pl-Pl")}
-      </td>
-      <td className="table__data">Liczba: {countListItems(data.list)}</td>
-      <td className="table__data">{data.status.toUpperCase()}</td>
-      <th className="table__data">Ikona</th>
-    </tr>
+    <>
+      <tr className="table__row">
+        <td className="table__data">
+          {new Date(data.date).toLocaleDateString("pl-Pl")}
+        </td>
+        <td className="table__data">Liczba: {countListItems(data.list)}</td>
+        <td className="table__data">{data.status.toUpperCase()}</td>
+        <th className="table__data">
+          <button
+            className="table__button"
+            onClick={() => {
+              setToggleActionMenu((prevState) => {
+                return !prevState;
+              });
+            }}
+          >
+            <div
+              className={` table__dots ${
+                toggleActionMenu ? "table__dots--rotate" : null
+              }`}
+            ></div>
+          </button>
+        </th>
+      </tr>
+      <ActionMenu active={toggleActionMenu} />
+    </>
   );
 }

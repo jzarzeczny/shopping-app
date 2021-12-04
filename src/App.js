8b59322bda0f16c211f361edb028fcae9d567222
jzarzeczny <jkzarzeczny@gmail.com>
2021-12-04 20:20:58 +0100

    initial v2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./sass/main.scss";
-import Main from "./pages/Main/Main";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "./context/FirebaseContext";
+import { ListsProvider } from "./context/ListContext";
+import Main from "./pages/Main/Main";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Lists from "./pages/Lists/Lists";
 import ShoppingList from "./pages/ShoppingList/ShoppingList";
 import Categories from "./pages/Categories/Categories";
-import { ListsProvider } from "./context/ListContext";
 import Account from "./pages/Account/Account";
+import NoMatch from "./pages/NoMatch/NoMatch";
 
 function App() {
   return (
@@ -16,6 +17,15 @@ function App() {
       <ListsProvider>
         <Router>
           <Switch>
+            <Route exact path="/">
+              <Main />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
             <Route path="/category">
               <Categories />
             </Route>
@@ -28,14 +38,8 @@ function App() {
             <Route path="/account">
               <Account />
             </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/">
-              <Main />
+            <Route path="*">
+              <NoMatch />
             </Route>
           </Switch>
         </Router>
